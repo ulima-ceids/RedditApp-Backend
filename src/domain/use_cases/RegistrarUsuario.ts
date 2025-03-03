64d097ae0ada4bbfb9ec1730b94c27f26c7d2974
Hernan Quintana <hernan@jsatch.com>
2025-03-03 15:47:52 -0500

    Implementando post

--- src/domain/use_cases/RegistrarUsuario.ts
+++ src/domain/use_cases/RegistrarUsuario.ts
@@ -1,3 +1,11 @@
-export const RegistrarUsuario = async () => {
+import { UsuarioRepository } from "../../repositories/UsuarioRepository"
 
+export const RegistrarUsuario = async (username: string, password: string, fechaNacimiento : Date,
+            genero : string,
+            codigoUlima : string,
+            foto : string,
+            telefono : string,
+            carreraId : number) => {
+    const usuario = await UsuarioRepository().addUser(username, password, fechaNacimiento, genero, codigoUlima, foto, telefono, carreraId)
+    return usuario
 }
--- src/repositories/UsuarioRepository.ts
+++ src/repositories/UsuarioRepository.ts
@@ -6,6 +6,15 @@ export const UsuarioRepository = () => {
         findByUsernamePassword : async (username: string, password: string) => {
             const usuario = await db.Usuario.findOne({ where: { username, password } });
             return usuario;
+        },
+        addUser : async (username: string, password: string, fechaNacimiento : Date,
+            genero : string,
+            codigoUlima : string,
+            foto : string,
+            telefono : string,
+            carreraId : number) => {
+            const usuario = await db.Usuario.create({ username, password, fechaNacimiento, genero, codigoUlima, foto, telefono, carreraId });
+            return usuario
         }
     }
 }
